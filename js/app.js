52bf28b950c3f26b40bbdee6935deeb470d48276
Oscar Guerrero <itsoscarg@icloud.com>
2017-02-10 19:02:27 -0500

    connect for complete

--- js/app.js
+++ js/app.js
@@ -3,10 +3,77 @@ console.log('app.js');
 var myGlobalConnectGame;
 
 $(document).ready(function () {
-  myGlobalConnectGame = new ConnectFour('Nizar', 'Javier');
+  var player1 = prompt('Enter name for Player #1');
+  var player2 = prompt('Enter name for Player #2');
+  myGlobalConnectGame = new ConnectFour(player1, player2);
 
   renderBoard();
+  updateCurrentPlayer();
+
+  $('#drop0').click(function () {
+    myGlobalConnectGame.playChecker(0);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+
+  $('#drop1').click(function () {
+    myGlobalConnectGame.playChecker(1);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+
+  $('#drop2').click(function () {
+    myGlobalConnectGame.playChecker(2);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+
+  $('#drop3').click(function () {
+    myGlobalConnectGame.playChecker(3);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+
+  $('#drop4').click(function () {
+    myGlobalConnectGame.playChecker(4);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+
+  $('#drop5').click(function () {
+    myGlobalConnectGame.playChecker(5);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+
+  $('#drop6').click(function () {
+    myGlobalConnectGame.playChecker(6);
+    renderBoard();
+    updateCurrentPlayer();
+  });
 });
+
+
+function updateCurrentPlayer () {
+  if (myGlobalConnectGame.currentPlayer === myGlobalConnectGame.player1) {
+    $('#current-player').html(myGlobalConnectGame.player1 + "'s Turn");
+    $('#current-player').addClass('player-1');
+    $('#current-player').removeClass('player-2');
+
+    $('#drop-buttons').addClass('turn-player-1');
+    $('#drop-buttons').removeClass('turn-player-2');
+  } else {
+    $('#current-player').html(myGlobalConnectGame.player2 + "'s Turn");
+    $('#current-player').addClass('player-2');
+    $('#current-player').removeClass('player-1');
+
+    $('#drop-buttons').addClass('turn-player-2');
+    $('#drop-buttons').removeClass('turn-player-1');
+  }
+
+  $('#current-player').css('visibility', 'visible');
+  $('#drop-buttons').css('visibility', 'visible');
+}
 
 
 function renderBoard () {
